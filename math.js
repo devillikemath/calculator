f22e76f1367839e5001b5696806d24bd795a7490
devil <160736919+devillikemath@users.noreply.github.com>
2025-02-06 20:53:35 +0800

    Update math.js

--- math.js
+++ math.js
@@ -8,10 +8,13 @@ function cleard(){
 }
 function calculateAnswer(){
     try{
-        let value=display.value.replace('×','*');
-        let value2=value.replace('÷','/');
-        console.log(value2);
-        display.value=eval(value2)
+        let value=display.value;
+        for(let i=0;i<value.length;i++){
+            value = value.replace('÷','/');
+            value = value.replace('×','*')
+        };
+        console.log(value);
+        display.value=variables(value)
     }catch(error){
         display.value="Error";
     }
@@ -19,3 +22,45 @@ function calculateAnswer(){
 function dele(){
     display.value=display.value.slice(0,-1);
 }
+function variables(variabl){
+    let variable = variabl.toString();
+    while(variable.match(/\+\+|[\+\-]{2}|[\-+]{2}/g)!==null){
+        if(variable.match(/--/)!==null){variable = variable.replace('--','+')}
+        if(variable.match(/\+\+/)!==null){variable = variable.replace('++','+')}
+        if(variable.match(/\+-/)!==null){variable = variable.replace('+-','-')}
+        if(variable.match(/-\+/)!==null){variable = variable.replace('-+','-')}}
+    let arry3 = variable.split(/[\+\-\/\*]/)
+    let something = -1;
+    let thing =-1
+    let variablearry =variable.split('')
+    let sum = 0;
+    arry3.forEach(element=>{
+        if(variable[variable.indexOf(element)-1]=='-'){
+            arry3[sum] = -element
+            variablearry[variablearry.indexOf(parseFloat(element))] = -element;
+        }
+        variable = variable.replace(element,'');
+        sum++;
+    })
+    variable =variablearry.join('')
+    arry3.forEach(element => {
+        something++;
+        let elements = parseFloat(element)
+        switch(variable[variable.indexOf(element)-1]){
+            case '*':
+                arry3[something] = arry3[something-1]*elements
+                arry3[something-1] = 0;
+                variable = variable.replace('*','')
+                break
+            case '/':
+                arry3[something] = arry3[something-1]/elements
+                arry3[something-1] = 0;
+                variable = variable.replace('/','')
+                break;
+        }
+        variable = variable.replace(element,'');
+    });
+    let total = 0;
+    arry3.forEach(element=>{total =total+parseFloat(element)})
+    return total;
+} 
